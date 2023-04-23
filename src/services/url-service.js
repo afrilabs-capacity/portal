1213f290d4d02ad3d03745f43e73c419bc548db7
afrilabs-capacity <factswarehouse360@gmail>
2023-04-23 16:59:33 +0100

    set production API url

--- src/services/url-service.js
+++ src/services/url-service.js
@@ -5,17 +5,17 @@ const urlService = () => {
   //     authUrl:"http://192.168.43.122:8080/laravel/site40/public/api/v1/auth/"
   // }
 
-  return {
-    baseUrl: "http://afrilabs.test/api/v1/",
-    publicUrl: "http://afrilabs.test/",
-    authUrl: "http://afrilabs.test/api/v1/auth/",
-  };
+  // return {
+  //   baseUrl: "http://afrilabs.test/api/v1/",
+  //   publicUrl: "http://afrilabs.test/",
+  //   authUrl: "http://afrilabs.test/api/v1/auth/",
+  // };
 
-  //   return {
-  //     baseUrl: "https://afrilabs-capacity.com/api/v1/",
-  //     publicUrl: "https://afrilabs-capacity.com/public",
-  //     authUrl: "https://afrilabs-capacity.com/api/v1/auth/",
-  //   };
+  return {
+    baseUrl: "https://afrilabs-capacity.com/api/v1/",
+    publicUrl: "https://afrilabs-capacity.com/public",
+    authUrl: "https://afrilabs-capacity.com/api/v1/auth/",
+  };
 };
 
 export default urlService;
